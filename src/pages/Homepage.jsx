3baf6a14af6de20c249a69da3c87a0780e608490
Devanada <ydevanada@gmail.com>
2022-03-23 10:51:44 +0700

    Added fetch data

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import axios from "axios";
 import { Layout } from "../components/Layout";
 import Header from "../components/Header";
 import "../styles/App.css";
@@ -7,10 +8,9 @@ export default class Homepage extends Component {
   constructor(props) {
     super();
     this.state = {
-      label: "save to reload (Class)",
       data: [],
       isReady: false,
-      idTodos: 0,
+      page: 1,
     };
   }
 
@@ -19,8 +19,42 @@ export default class Homepage extends Component {
   }
 
   async fetchData() {
-    const datas = JSON.parse(localStorage.getItem("data"));
-    this.setState({ data: datas ? datas : [], isReady: true });
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${this.state.page}`
+      )
+      .then((response) => {
+        this.setState({ data: response.data.results, isReady: true });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // fetch(
+    //   `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${this.state.page}`
+    // )
+    //   .then((response) => response.json())
+    //   .then((res) => {
+    //     this.setState({ data: res.results, isReady: true });
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  }
+
+  async addFavorite(item) {
+    let getLocal = JSON.parse(localStorage.getItem("favorites"));
+    if (getLocal) {
+      /*
+      Bikin variabel find untuk mengecek apakah item yang diklik sudah ada di local storage
+      const findIsExist = (pakai function find)
+      Setelah mencari, buat satu conditional ketika findIsExist ada, maka item tidak di push, melainkan di remove, selain itu 
+      berarti di push
+      */
+      getLocal.push(item);
+      localStorage.setItem("favorites", JSON.stringify(getLocal));
+    } else {
+      localStorage.setItem("favorites", JSON.stringify([item]));
+    }
   }
 
   render() {
@@ -29,7 +63,28 @@ export default class Homepage extends Component {
         <div className="main-con">
           <Header />
           <Layout>
-            <p className="text-white">Test</p>
+            <h1 className="text-white text-5xl">Now Playing</h1>
+            <div className="grid grid-flow-row-dense grid-cols-4 grid-rows-4">
+              {this.state.data.map((item) => {
+                return (
+                  <div key={item.id} className="grow m-5">
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                      alt={item.poster_path}
+                    />
+                    <p className="text-center text-white font-bold text-xl">
+                      {item.title}
+                    </p>
+                    <button
+                      className="bg-indigo-500 rounded-none"
+                      onClick={() => this.addFavorite(item)}
+                    >
+                      Favorite
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
           </Layout>
         </div>
       );
