b1a7a4a2490318649e10e80fde290ca090f5c37b
Devanada <ydevanada@gmail.com>
2022-04-02 22:04:01 +0700

    Rebranding, added easter egg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSun, FaMoon } from "react-icons/fa";
 import { ThemeContext } from "../utils/context";
 import logo from "../assets/logo.svg";
 import "../styles/Header.css";
 
 const Header = (props) => {
+  const navigate = useNavigate();
   const { theme, setTheme } = useContext(ThemeContext);
 
   const handleThemeChange = (mode) => {
@@ -15,19 +16,28 @@ const Header = (props) => {
 
   return (
     <nav className="sticky top-0 w-full border-gray-200 px-2 sm:px-4 py-2.5 bg-zinc-800">
-      <div className="container flex flex-wrap justify-between items-center mx-auto">
+      <div className="container flex items-center grid grid-flow-row grid-cols-3  mx-auto">
         <Link to="/">
           <h1 className="text-white text-2xl font-bold">Cinephile</h1>
         </Link>
-        <img src={logo} className="header-logo" alt="logo" />
-        <div className="flex justify-center items-center space-x-4">
+        <div
+          className="flex justify-center"
+          onClick={() => navigate("/nowhere")}
+        >
+          <img
+            src={logo}
+            className="header-logo place-self-center"
+            alt="logo"
+          />
+        </div>
+        <div className="flex justify-end items-center space-x-4">
           {/* <p className="text-white">My Favorite Movies</p> */}
-          <button
+          {/* <button
             className="bg-neutral-500 hover:bg-neutral-600 rounded text-white font-bold p-2"
             onClick={props.onClick}
           >
             Login
-          </button>
+          </button> */}
           {theme === "dark" ? (
             <FaSun
               className="w-8 h-8 text-white"
